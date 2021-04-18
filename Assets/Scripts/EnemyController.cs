a96f05cec00e9b019219a1baa72ddd26a06e4ef6
MykailahT96 <77352972+MykailahT96@users.noreply.github.com>
2021-04-17 21:18:16 -0500

    InitialCommit
    the beginning of the end

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+public class EnemyController : MonoBehaviour
+{
+    
+    public NavMeshAgent enemyAgent;
+
+    public ThirdPersonCharacter enemyCharacter;
+
+    void Start()
+    {
+        agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RotateCoin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotateCoin : MonoBehaviour
+{
+    public float spinSpeed = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, (spinSpeed * Time.deltaTime), 0f);
+    }
+}
